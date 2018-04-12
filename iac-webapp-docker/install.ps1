d0cf86f66da28ad5f5a655514a1b339927d4d721
Daniel Scholl <daniel.scholl@microsoft.com>
2018-04-12 10:12:16 -0500

    Added Linux Web App Containers

---
+++ iac-webapp-docker/install.ps1
@@ -0,0 +1,50 @@
+<#
+.SYNOPSIS
+  Infrastructure as Code Component
+.DESCRIPTION
+  Install a Linux WebApp
+.EXAMPLE
+  .\install.ps1
+  Version History
+  v1.0   - Initial Release
+#>
+#Requires -Version 5.1
+#Requires -Module @{ModuleName='AzureRM.Resources'; ModuleVersion='5.0'}
+
+Param(
+  [string]$Subscription = $env:AZURE_SUBSCRIPTION,
+  [string]$ResourceGroupName = $env:AZURE_GROUP,
+  [string]$Location = $env:AZURE_LOCATION,
+  [string]$DockerImage = "danielscholl/web-angular-demo",
+  [string]$DockerPort = "3000"
+)
+
+if (Test-Path ..\scripts\functions.ps1) { . ..\scripts\functions.ps1 }
+if (Test-Path .\scripts\functions.ps1) { . .\scripts\functions.ps1 }
+if ( !$Subscription) { throw "Subscription Required" }
+if ( !$ResourceGroupName) { throw "ResourceGroupName Required" }
+if ( !$Location) { throw "Location Required" }
+
+###############################
+## Azure Initialize           ##
+###############################
+$BASE_DIR = Get-ScriptDirectory
+$DEPLOYMENT = Split-Path $BASE_DIR -Leaf
+LoginAzure
+CreateResourceGroup $ResourceGroupName $Location
+
+Write-Color -Text "Registering Provider..." -Color Yellow
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.Web
+
+##############################
+## Deploy Template          ##
+##############################
+Write-Color -Text "`r`n---------------------------------------------------- "-Color Yellow
+Write-Color -Text "Deploying ", "$DEPLOYMENT ", "template..." -Color Green, Red, Green
+Write-Color -Text "---------------------------------------------------- "-Color Yellow
+New-AzureRmResourceGroupDeployment -Name $DEPLOYMENT `
+  -TemplateFile $BASE_DIR\azuredeploy.json `
+  -TemplateParameterFile $BASE_DIR\azuredeploy.parameters.json `
+  -prefix $ResourceGroupName `
+  -containerImage $DockerImage -openPort $DockerPort `
+  -ResourceGroupName $ResourceGroupName
